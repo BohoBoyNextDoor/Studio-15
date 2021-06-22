7c1299ababf6f7e5365dac23b40e1faa8c6e31cd
Mason Oltermann <m.oltermann@gmail.com>
2021-06-22 12:13:09 -0500

    this is a name

---
+++ Studio 15 the real one/Studio 15 the real one/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Studio_15_the_real_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("yo whattup");
+        }
+    }
+}
